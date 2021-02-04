6c27c17dee7bc6614e4572af787cc6b14ef2c15c
Luís Clício <cliciosa14@gmail.com>
2021-02-04 14:04:14 -0300

    feat: make it possible to choose animations via props in the Wrapper component

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -2,6 +2,22 @@ import { ReactNode } from 'react';
 
 import '../styles/components/Wrapper.css';
 
-export default function Wrapper({ children }: { children: ReactNode }) {
-  return <main className="wrapper">{children}</main>;
+interface WrapperProps {
+  animation?: {
+    appear?: boolean;
+    move?: boolean;
+  };
+  children: ReactNode;
+}
+
+export default function Wrapper({ animation = {}, children }: WrapperProps) {
+  return (
+    <main
+      className={`wrapper ${animation.appear && 'wrapper--appear'} ${
+        animation.move && 'wrapper--move'
+      }`}
+    >
+      {children}
+    </main>
+  );
 }
