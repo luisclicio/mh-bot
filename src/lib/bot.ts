72e21cf564d98fcc5d417be63021d015d2d25710
Luís Clício <cliciosa14@gmail.com>
2021-02-05 12:25:43 -0300

    feat: create Bot library

---
+++ src/lib/bot.ts
@@ -0,0 +1,24 @@
+export interface QueryItem {
+  pattern: RegExp;
+  response: (message?: string) => string;
+}
+
+export default function Bot(queries: QueryItem[], fallbackMessage: string) {
+  return (message: string) => {
+    let responses: string[] = [];
+
+    queries.forEach((query) => {
+      if (query.pattern.test(message)) {
+        responses.push(query.response(message));
+      }
+    });
+
+    const responseFound = responses.length > 0;
+
+    if (!responseFound) {
+      responses.push(fallbackMessage);
+    }
+
+    return responses;
+  };
+}
